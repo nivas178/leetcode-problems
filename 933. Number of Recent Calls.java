42f1f590becc1cfe4ee925462f7febfb05b7152d
nivas178 <meriganivas@gmail.com>
2026-07-27 11:10:39 +0530

    Implement RecentCounter class for tracking requests

---
+++ 933. Number of Recent Calls.java
@@ -0,0 +1,19 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+class RecentCounter {
+    private Queue<Integer> requests;
+
+    public RecentCounter() {
+        requests = new LinkedList<>();
+    }
+    
+    public int ping(int t) {
+        requests.add(t);
+        while (requests.peek() < t - 3000) {
+            requests.poll();
+        }
+        
+        return requests.size();
+    }
+}
